42eb73a04bf129b8845f38a3b7c4249b63623d19
htetyadanar-hyo <149476965+htetyadanar-hyo@users.noreply.github.com>
2024-05-04 20:21:27 -0500

    Add files via upload

---
+++ FPP_Assignment2/Prog4.java
@@ -0,0 +1,15 @@
+package FPP_Assignment2;
+
+public class Prog4 {
+    public static void main(String[] args) {
+        float a = 1.27f;
+        float b = 3.881f;
+        float c = 9.6f;
+
+        int result1 = (int) (a + b + c);
+        int result2 = Math.round(a+b+c);
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
---
+++ FPP_Assignment2/RemoveDups.java
@@ -0,0 +1,42 @@
+package FPP_Assignment2;
+
+public class RemoveDups {
+
+    public static void main(String[] args) {
+        String[] myArr = {"horse", "dog", "dog", "horse", "cat"};
+        String[] result = remove(myArr);
+        for(String i: result){
+            System.out.println(i);
+        }
+    }
+
+    static String[] remove(String[] a){
+        int count = 0;
+        for(int i=0; i<a.length-1; i++){
+            for(int j=i+1; j<a.length; j++){
+                if(a[i].equals(a[j])){
+                    a[j] = "";
+                }
+                else if(a[i] == ""){
+                    continue;
+                }
+            }
+        }
+
+        for(int i=0;i<a.length; i++){
+            if(a[i] != ""){
+                count++;
+            }
+        }
+
+        String[] result = new String[count];
+        int j = 0;
+        for(int i=0;i<a.length;i++){
+            if(a[i] != ""){
+                result[j] = a[i];
+                j++;
+            }
+        }
+        return result;
+     }
+}
