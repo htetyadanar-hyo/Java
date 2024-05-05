539c2bce3d7b7ab922ffcaa8ba28836be9187348
htetyadanar-hyo <149476965+htetyadanar-hyo@users.noreply.github.com>
2024-05-04 20:16:15 -0500

    Add files via upload

---
+++ Assignment4/CountNumber.java
@@ -0,0 +1,15 @@
+package Assignment4;
+
+public class CountNumber {
+    public static void main(String[] args) {
+        int n = 1234;
+        System.out.println(count(n));
+    }
+    static int count(int n){
+        if(n < 10){
+            return 1;
+        }
+        int result = 1+count(n/10);
+        return result;
+    }
+}
---
+++ Assignment4/SumArray.java
@@ -0,0 +1,23 @@
+package Assignment4;
+
+import javax.swing.plaf.metal.MetalLookAndFeel;
+
+public class SumArray {
+
+    public static void main(String[] args) {
+        int[] n = {1,2,3};
+        System.out.println(sum(n));
+    }
+
+    static int sum(int[] a){
+        if(a.length ==0){
+            return 0;
+        }
+        else if(a.length == 1){
+            return a[0];
+        }
+        int[] newArray = new int[a.length-1];
+        System.arraycopy(a,1,newArray,0,a.length-1);
+        return a[0]+sum(newArray);
+    }
+}
